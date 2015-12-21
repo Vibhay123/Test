de39a33ce0774af2819a86ee76a6322baba98411
Vibhay Sachan <vibiit@gmail.com>
2015-12-21 17:42:38 +0530

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['username'] = 'admin9WcTNZI';
+$db['default']['password'] = 'r97xW1HprEy-';
 $db['default']['database'] = 'login';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
